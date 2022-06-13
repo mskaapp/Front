f440538ed8dbe94d907ef869fa8b0c0c8faf6f86
MSKA <mednologic@gmail.com>
2022-06-13 20:23:38 +0200

    "Some changes on routing"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,16 +6,32 @@ import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
 import { AboutComponent } from './about/about.component';
 import { CalendarComponent } from './calendar/calendar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileRecruiterComponent } from './profile-recruiter/profile-recruiter.component';
+import { ProfileTecnicComponent } from './profile-tecnic/profile-tecnic.component';
+import { RegisterComponent } from './register/register.component';
+import { RegisterRecruiterComponent } from './register-recruiter/register-recruiter.component';
+import { RegisterTecnicComponent } from './register-tecnic/register-tecnic.component';
+import { WorksComponent } from './works/works.component';
 
 import { AuthGuard } from './guards/auth.guard';
 
 //if its with canActivate : [AuthGuard], means that only can access if the user its logged in
 const routes: Routes = [
+  //Out of security
   {path: '', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  //Inside Security
   {path: 'navbar', component: NavbarComponent,canActivate : [AuthGuard]},
   {path: 'main', component: MainComponent, canActivate : [AuthGuard]},
   {path: 'about', component: AboutComponent, canActivate : [AuthGuard]},
-  {path: 'calendar', component: CalendarComponent, canActivate : [AuthGuard]}
+  {path: 'calendar', component: CalendarComponent, canActivate : [AuthGuard]},
+  {path: 'profile', component: ProfileComponent, canActivate : [AuthGuard]},
+  {path: 'profileRecruiter', component: ProfileRecruiterComponent, canActivate : [AuthGuard]},
+  {path: 'profileTecnic', component: ProfileTecnicComponent, canActivate : [AuthGuard]},
+  {path: 'registerRecruiter', component: RegisterRecruiterComponent, canActivate : [AuthGuard]},
+  {path: 'registerTecnic', component: RegisterTecnicComponent, canActivate : [AuthGuard]},
+  {path: 'works', component: WorksComponent, canActivate : [AuthGuard]},
 ];
 
 @NgModule({
