0cffad550ba1a1644a4046d5197b4c593f8cfe3d
DavidDalmauDieguez <david.dieguez13@gmail.com>
2022-06-16 09:09:15 +0200

    modelos reclutador y tecnico

---
+++ src/app/models/reclutadorModel.ts
@@ -0,0 +1,36 @@
+export class usuarioModel {
+  private id: number;
+  private experiencia: number;
+  private nivel: number;
+  private web: string;
+  private tipo_reclutador: string;
+
+  constructor(id: number, experiencia: number, nivel: number, web: string, tipo_reclutador: string){
+      this.id = id;
+      this.experiencia = experiencia;
+      this.nivel = nivel;
+      this.web = web;
+      this.tipo_reclutador = tipo_reclutador;
+
+  }
+
+  getId(): number {
+    return this.id;
+  }
+
+  getExperiencia(): number{
+    return this.experiencia;
+  }
+
+  getNivel(): number{
+    return this.nivel;
+  }
+
+  getGithub(): string{
+    return this.web;
+  }
+
+  getPinterest(): string{
+    return this.tipo_reclutador;
+  }
+}
---
+++ src/app/models/tecnicoModel.ts
@@ -0,0 +1,55 @@
+export class usuarioModel {
+  private id: number;
+  private experiencia: number;
+  private nivel: number;
+  private github: string;
+  private pinterest: string;
+  private trabajo_en_curso: number;
+  private trabajos_finalizados: number;
+  private tipo_tecnico: string;
+
+  constructor(id: number, experiencia: number, nivel: number, github: string, pinterest: string, trabajo_en_curso: number,
+    trabajos_finalizados: number, tipo_tecnico: string){
+      this.id = id;
+      this.experiencia = experiencia;
+      this.nivel = nivel;
+      this.github = github;
+      this.pinterest = pinterest;
+      this.trabajo_en_curso = trabajo_en_curso;
+      this.trabajos_finalizados = trabajos_finalizados;
+      this.tipo_tecnico = tipo_tecnico;
+
+  }
+
+  getId(): number {
+    return this.id;
+  }
+
+  getExperiencia(): number{
+    return this.experiencia;
+  }
+
+  getNivel(): number{
+    return this.nivel;
+  }
+
+  getGithub(): string{
+    return this.github;
+  }
+
+  getPinterest(): string{
+    return this.pinterest;
+  }
+
+  getTrabajo_en_curso(): number{
+    return this.trabajo_en_curso;
+  }
+
+  getTrabajos_finalizados(): number{
+    return this.trabajos_finalizados;
+  }
+
+  getTipo_tecnico(): string{
+    return this.tipo_tecnico;
+  }
+}
