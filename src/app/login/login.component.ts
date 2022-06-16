d49a2853b4eb8a68938a1cf4dbb20ba0f1ad4a9c
MSKA <mednologic@gmail.com>
2022-06-16 08:05:21 +0200

    "Remember me working"

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,17 +12,33 @@ export class LoginComponent implements OnInit {
 
   isFormValid = false;
   areCredentialsInvalid = false;
+  chRememberMe!:  boolean;
+  tfUser!:        string;
+  tfPassword!:     string;
 
   constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
+    console.log("Estatus check local"+localStorage.getItem('checked'));
+    console.log("Estatus check"+localStorage.getItem('checked'));
+    //LocalStorage cant storage booleans, the string must be parsed
+    if(localStorage.getItem('checked')=='true'){
+      //Setting checkbox checked
+      this.chRememberMe   = true;
+      this.tfUser         = localStorage.getItem('user');
+      this.tfPassword     = localStorage.getItem('password');
 
-
+      //TODO: ser values of textfield from localstorage
+    }else{
+      //Setting checkbox unchecked
+      this.chRememberMe = false;
+    }
   }
 
+
   onSubmit(signInForm: NgForm) {
-    console.log(signInForm.value);
-    console.log(signInForm.valid);
+    //console.log(signInForm.value);
+    //console.log(signInForm.valid);
     if (!signInForm.valid) {
       this.isFormValid = true;
       this.areCredentialsInvalid = false;
@@ -39,8 +55,18 @@ export class LoginComponent implements OnInit {
       this.areCredentialsInvalid = true;
     }
   }
-  private setUserPass(usuario: string, password:string){
-    localStorage.setItem('user',usuario);
-    localStorage.setItem('password', password);
+  //For remember user and password
+  // add usuario: string, password:string
+  public changestatusRemember(){
+    if(this.chRememberMe == true){
+      localStorage.setItem('checked', 'true');
+      localStorage.setItem('user', this.tfUser );
+      localStorage.setItem('password', this.tfPassword);
+    }else{
+      localStorage.setItem('checked', 'false');
+      localStorage.setItem('user', '' );
+      localStorage.setItem('password', '');
+    }
+    console.log("changed");
   }
 }
