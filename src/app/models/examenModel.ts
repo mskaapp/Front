8c1a5e7f6896093dbafbd55f9117e69ea2150b54
DavidDalmauDieguez <david.dieguez13@gmail.com>
2022-06-16 10:47:35 +0200

    modelo examen

---
+++ src/app/models/examenModel.ts
@@ -0,0 +1,42 @@
+export class comentarioTrabajoModel{
+  private id: number;
+  private nombre: string;
+  private tipo: string;
+  private descripcion: string;
+  private cantidad_exp: number;
+  private fk_id_skill: number;
+
+  constructor(id: number, nombre: string, tipo: string, descripcion: string, cantidad_exp: number, fk_id_skill: number){
+    this.id = id;
+    this.nombre = nombre;
+    this.tipo = tipo;
+    this.descripcion = descripcion;
+    this.cantidad_exp = cantidad_exp;
+    this.fk_id_skill = fk_id_skill;
+
+  }
+
+  getId(): number {
+    return this.id;
+  }
+
+  getNombre(): string{
+    return this.nombre;
+  }
+
+  getTipo(): string {
+    return this.tipo;
+  }
+
+  getDescripcion(): string {
+    return this.descripcion;
+  }
+
+  getCantidad_exp(): number {
+    return this.cantidad_exp;
+  }
+
+  getFk_id_skill(): number {
+    return this.fk_id_skill;
+  }
+}
