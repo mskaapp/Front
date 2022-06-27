cfb5112c5711a40ce294beb86ee3f831619760a3
Ixabel Justo <miju355@vidalibarraquer.net>
2022-06-27 11:07:34 +0200

    Paginate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 //Components
 import { AppRoutingModule } from './app-routing.module';
@@ -60,6 +61,7 @@ import { HttpClientModule } from '@angular/common/http';
 
    ],
   imports: [
+    NgbModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
--- src/app/html/main/main.component.ts
+++ src/app/html/main/main.component.ts
@@ -3,18 +3,36 @@ import { HttpClient } from '@angular/common/http';
 import { usuarioModel } from 'src/app/models/usuarioModel';
 import { postUsuarioModel } from 'src/app/models/postUsuarioModel';
 
+import {NgbPaginationConfig} from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
+  providers: [NgbPaginationConfig]
 })
 export class MainComponent implements OnInit {
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    // private route: ActivatedRoute,
+    private config: NgbPaginationConfig
+    // private router: Router
+    ) {
+
+      this.config.boundaryLinks = true;
+
+    }
 
   screenWidth       = screen.width;
   screenHeight      = screen.height;
   usuario!:         usuarioModel;
   post!:            postUsuarioModel[];
+  totalItems: number;
+  page: number;
+  previousPage: number;
+  showPagination: boolean;
+  path: any;
+  order: any;
 
   // pageActual: number = 1;
   screenSize() {
@@ -24,7 +42,11 @@ export class MainComponent implements OnInit {
   ngOnInit(): void {
     this.screenSize();
     this.getPosts();
+    this.page =1;
+    this.previousPage =1;
+    this.fillPosts(1);
   }
+
   getUserById(){
     //TODO: capture the id from field html and sustitute for 1
     this.httpClient.get<any>('https://myskillaround-spring-testing.herokuapp.com/api/usuario/1').subscribe(
@@ -43,4 +65,41 @@ export class MainComponent implements OnInit {
     }
   );
   }
+
+  sortTable(prop: string) {
+    this.path = prop.split('.');
+    this.order = this.order * (-1);
+    return false;
+  }
+
+
+  fillPosts(page : number) : void {
+    this.httpClient.get<any>('https://myskillaround-spring-testing.herokuapp.com/api/postusuario/').subscribe(
+
+		  data => {
+			if ((!data && !data.result) || (data && data.result && data.result.length ==0)) {
+			  this.post = [];
+			  this.showPagination = false;
+        this.post = data;
+			}
+			else {
+			  this.post = data.result;
+			  this.totalItems = data.totalAmount;
+			  this.showPagination = true;
+			}
+
+		  },
+		  error => {
+			// Aquí se debería tratar el error, bien mostrando un mensaje al usuario o de la forma que se desee.
+		  }
+		);
+  }
+
+  loadPage(page: number) {
+    if (page !== this.previousPage) {
+      this.previousPage = page;
+      this.fillPosts(this.page-1);
+    }
+  }
+
 }
