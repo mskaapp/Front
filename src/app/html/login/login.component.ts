b33bfd478efcbc025764001c668644ed9592b258
MSKA <mednologic@gmail.com>
2022-06-27 07:49:07 +0200

    "cors"

--- src/app/html/login/login.component.ts
+++ src/app/html/login/login.component.ts
@@ -73,7 +73,7 @@ export class LoginComponent implements OnInit {
   }
   getUserById(){
     //TODO: capture the id from field html and sustitute for 1
-    this.httpClient.get<any>('https://myskillaround-spring-testing.herokuapp.com/api/usuario/1').subscribe(
+    this.httpClient.get<any>('/api/usuario/1').subscribe(
       response =>{
         console.log(response);
         this.usuario=response;
